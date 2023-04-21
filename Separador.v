02f86f4c9f78c6b78b31eaea9ecc6aea4bb968b4
ericksuzart <erick.suzart@ufba.br>
2023-04-21 19:27:17 -0300

    Fix in code-base

--- Separador.v
+++ Separador.v
@@ -5,14 +5,14 @@ output reg [15:0] saida_16;
 
 always @ (*)
 begin
-	case (seletor_entrada)
-		0:	if (seletor_palavra == 0) 
-				saida_16 = entrada_0[31:16];
-			else saida_16 = entrada_0[15:0];
-		1:	if (seletor_palavra == 1) 
-		      saida_16 = entrada_1[31:16];
-			else saida_16 = entrada_1[15:0];			
-	endcase
+   case (seletor_entrada)
+      0: if (seletor_palavra == 0) 
+            saida_16 = entrada_0[31:16];
+         else saida_16 = entrada_0[15:0];
+      1: if (seletor_palavra == 1) 
+            saida_16 = entrada_1[31:16];
+         else saida_16 = entrada_1[15:0];
+   endcase
 end
 
 endmodule 
